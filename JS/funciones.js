4cc0c82a24fa434ed76731afed93bb3c2d4d1ec6
JavierPinoCastillo <javier.pino.castillo[1@gmail.com>
2022-03-22 21:16:44 -0300

    Mi primer commit

---
+++ JS/funciones.js
@@ -0,0 +1,9 @@
+console.log("page loading...");
+
+var likeCount = 3;
+var likeSpan1 = document.querySelector("#post1");
+
+function like(){
+    likeCount++;
+    likeSpan1.innerText = likeCount + " like(s)";
+}
